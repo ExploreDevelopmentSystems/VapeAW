c9ae147f5863a5a9387f83b0b5efb150d30102d5
ExploreDevelopmentSystems <mickshaymusic@gmail.com>
2024-06-18 01:21:30 -0400

    Update autoclicker.lua

--- main/modules/autoclicker.lua
+++ main/modules/autoclicker.lua
@@ -1,30 +1,28 @@
-
 local rangeModule = {}
-
-
 local active = false
-
+local rangeValue = 10
 
 function rangeModule.start()
     active = true
     print("Range module started")
 
-end
 
+end
 
 function rangeModule.end()
     active = false
     print("Range module ended")
-
 end
-
 
 function rangeModule.update()
     if active then
-
         print("Range module is updating")
     end
 end
 
+function rangeModule.setRange(value)
+    rangeValue = value
+    print("Range set to", rangeValue)
+end
 
 return rangeModule
